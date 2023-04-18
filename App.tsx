3c63d57b463c8d6057d0252820edf90cd6a14415
Tiotedd <andrelsbrito2022@gmail.com>
2023-04-17 23:46:42 -0300

    working on Desing Systems and Desing Guide

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
+import {ThemeProvider} from 'styled-components'
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+import theme from '@theme/index';
+
 import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading';
 
 export default function App() {
+  const [fontsLoaded] =useFonts({Roboto_400Regular,Roboto_700Bold});
+
   return (
-    <Groups/>
+    <ThemeProvider theme={theme}>
+       {fontsLoaded ? <Groups/> : <Loading/>}
+    </ThemeProvider>
+   
   );
 }
 
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,19 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {Container}  from './styles';
 
 export function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-      
-    </View>
+    <Container>
+  
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
