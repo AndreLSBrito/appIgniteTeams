7ad1103fbc1e792d7fca9b9fceb886518897b5a8
Tiotedd <andrelsbrito2022@gmail.com>
2023-04-18 23:29:35 -0300

    creating the first screen Groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,20 @@
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+
 import {Container}  from './styles';
+import { GroupCard } from '@components/GroupCard';
+
 
 export function Groups() {
   return (
     <Container>
-  
+      <Header />
+      
+      <Highlight 
+        title='Turmas'
+        subtitle='jogue com a sua turma'
+      />
+      <GroupCard title='Galera do Ignite'/>
     </Container>
   );
 }
