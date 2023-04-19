d36cf295e19a06a4d90d1d2fefd709a2f04941c8
Tiotedd <andrelsbrito2022@gmail.com>
2023-04-18 23:31:26 -0300

    style StatusBar

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from 'react-native';
 import {ThemeProvider} from 'styled-components'
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 import theme from '@theme/index';
@@ -10,7 +11,12 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-       {fontsLoaded ? <Groups/> : <Loading/>}
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups/> : <Loading/>}
     </ThemeProvider>
    
   );
