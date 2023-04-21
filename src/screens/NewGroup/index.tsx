b01fa98a081a4534c53063cb458c25bc10bfdd1b
Tiotedd <andrelsbrito2022@gmail.com>
2023-04-21 20:53:53 -0300

    Created NewGroup screen

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@components/Header";
 import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input/index";
 
 
 
@@ -17,9 +18,12 @@ export function NewGroup(){
           title="Nova turma"
           subtitle="Crie a turma para adicionar as pessoas"
         />
-
+        <Input
+          placeholder={'Nome da turma'}
+        />
         <Button
           title="Criar"
+          style={{marginTop: 20}}
         />
       </Content>
     </Container>
