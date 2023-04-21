1e043c034c711049b7cb2243d613d8aab20d134f
Tiotedd <andrelsbrito2022@gmail.com>
2023-04-21 20:52:07 -0300

    created Input Component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,16 @@
+import { TextInputProps } from "react-native";
+
+import { Container } from "./styles";
+import { useTheme } from "styled-components/native";
+
+export function Input({...rest}: TextInputProps){
+
+  const {COLORS} = useTheme()
+
+  return(
+    <Container
+      placeholderTextColor={COLORS.GRAY_300}
+      {...rest}
+    />
+  )
+}
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,18 @@
+import { TextInput } from "react-native";
+import styled, {css} from "styled-components/native";
+
+export const Container = styled(TextInput)`
+  flex: 1;
+  min-height: 56px;
+  max-height: 56px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    background-color: ${theme.COLORS.GRAY_700};
+    font-size: ${theme.FONT_SIZE.MD}px;
+    color: ${theme.COLORS.WHITE};
+  `};
+
+  border-radius: 6px;
+  padding: 16px;
+`;
