2a2bd9d399ff9afeeccccf93f47871070a502f35
Tiotedd <andrelsbrito2022@gmail.com>
2023-04-21 20:55:30 -0300

    testing screen

--- App.tsx
+++ App.tsx
@@ -4,7 +4,9 @@ import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/ro
 import theme from './src/theme';
 
 import { Groups } from '@screens/Groups';
+
 import { Loading } from '@components/Loading';
+import { Players } from '@screens/Players';
 
 
 
@@ -18,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups/> : <Loading/>} 
+      {fontsLoaded ? <Players/> : <Loading/>} 
     </ThemeProvider>
    
   );
