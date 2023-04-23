32b422d2e19b7c3bf15a36de1b7f15a935f64564
Tiotedd <andrelsbrito2022@gmail.com>
2023-04-22 21:32:27 -0300

    Added routes and navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'react-native';
-import {ThemeProvider} from 'styled-components'
+import {ThemeProvider} from 'styled-components/native'
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 import theme from './src/theme';
 
-import { Groups } from '@screens/Groups';
 
 import { Loading } from '@components/Loading';
-import { Players } from '@screens/Players';
+
+import { Routes } from '@routes/index';
 
 
 
@@ -20,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Players/> : <Loading/>} 
+      {fontsLoaded ? <Routes/> : <Loading/>} 
     </ThemeProvider>
    
   );
