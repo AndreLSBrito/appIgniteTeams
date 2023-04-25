9504f76ca315e3a965ed6c3accb8220c4eacfdb9
Tiotedd <andrelsbrito2022@gmail.com>
2023-04-25 00:12:55 -0300

    refactoring component Loading

--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -7,7 +7,7 @@ export const Container = styled.View`
   justify-content: center;
   align-items:center;
 
-  background-color: ${({theme})=>theme.COLORS.GRAY_700};
+  background-color: ${({theme})=>theme.COLORS.GRAY_600};
 `;
 
 export const LoadIndicator = styled.ActivityIndicator.attrs(({theme}) => ({
