1385ab9c0b774ae427a18c9e0f967cf6f736ea30
Tiotedd <andrelsbrito2022@gmail.com>
2023-04-25 00:12:31 -0300

    added references to control focus and kayboard

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,14 +1,19 @@
-import { TextInputProps } from "react-native";
-
-import { Container } from "./styles";
+import { TextInput, TextInputProps } from "react-native";
 import { useTheme } from "styled-components/native";
 
-export function Input({...rest}: TextInputProps){
+import { Container } from "./styles";
+
+type Props = TextInputProps & {
+  inputRef?: React.RefObject<TextInput>;
+}
+
+export function Input({inputRef, ...rest}: Props){
 
   const {COLORS} = useTheme()
 
   return(
     <Container
+      ref={inputRef}
       placeholderTextColor={COLORS.GRAY_300}
       {...rest}
     />
